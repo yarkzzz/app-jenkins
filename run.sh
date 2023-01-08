522e9eaf116ac6ed55c66fe41df05cec8e14df61
yarkzzz <80464422+yarkzzz@users.noreply.github.com>
2023-01-08 18:37:01 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/env sh
+
 set -x
-npm start &
+npm run build
+set +x
+
+set -x
+npm start & curl http://localhost:8065 &
 sleep 1
 echo $! > .pidfile
 set +x
 
-echo 'Visit http://domain:38065 to see your Node.js application in action.'
+echo 'Visit http://domain:8065 to see your Node.js application in action.'
