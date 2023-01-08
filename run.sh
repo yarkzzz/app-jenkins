089d162c2592f8f61a027a82b1c2de5f525362ed
yarkzzz <80464422+yarkzzz@users.noreply.github.com>
2023-01-08 18:14:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env sh
 set -x
-node app.js &
+npm start &
 sleep 1
 echo $! > .pidfile
 set +x
 
-echo 'Visit http://domain:8065 to see your Node.js/React application in action.'
+echo 'Visit http://domain:3000 to see your Node.js/React application in action.'
